569973d19a8848a4aae850b752a34825c63fd3c7
Vitaly Dmitriev <vitalik.dmitriev1995@gmail.com>
2018-10-01 14:13:27 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var div = 2;
+    var res = 0;
+    var multiplier = [];
+    var count = 0;
+    while (base > 1)
+    {
+        while (base % div == 0)
+        {
+            base = base / div;
+            multiplier.push(div);
+        }
+        div++;
+    }
+    
+    for(var i = 0; i < multiplier.length; ++i){
+        if(multiplier[i] == multiplier[multiplier.length - 1])
+            count++;
+    }
+
+    
+    while(number > 0)
+    {
+        if(number>1){
+            number /= div-1;
+            res += Math.floor(number);
+        }else{
+            break;
+        }
+    }
+    return Math.floor(res/count);
 }
